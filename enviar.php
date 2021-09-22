6c61efde7ab5d962bff9badda07cb64301557c33
DukO7 <89145197+DukO7@users.noreply.github.com>
2021-09-21 19:05:22 -0500

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,10 @@
+<?php
+$destino="lechuga.lflr@gmail.com"
+$nombre=$_Post["nombre"];
+$correo=$_Post["correo"];
+$telefono=$_Post["telefono"];
+$mensaje=$_Post["mensaje"];
+$contenido ="Nombre: " . $nombre . "\nCorreo" . $correo . "\nTelefono" . $telefono . "\nMensaje" . $mensaje;
+ mail($destino, "Contacto",$contenido);
+ header("Location:mensaje.html")
+?>
